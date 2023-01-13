72ee2a6e857f5d5aba4dcfa4f14e919f9f49ffd6
Jaskiran Singh <jsingh8986@yahoo.com>
2023-01-12 23:01:58 -0800

    commit tree 8572d08e91cc1f9f9731c274e8d201a48c904377

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,19 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // console.log('before', count)
+    //   setCount((currentCount) => currentCount + 1);
+    // console.log('middle', count)
+    //   setCount((currentCount) => currentCount + 1);  
+    //   console.log('after', count)
+
+      console.log('before', count)
+        setCount(count + 2);
+      console.log('middle', count)
+      //   setCount(count + 2);  
+      //   console.log('after', count)
+    
+  
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
